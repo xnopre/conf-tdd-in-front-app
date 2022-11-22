43d6747cb97de4f713c05b47906ddcbeb7ff1923
Xavier NOPRE <xavier.nopre@vrtice.com>
2022-11-22 15:01:06 +0100

    Départ live coding démo

--- src/components/PreviewExpe/PreviewExpe.test.tsx
+++ src/components/PreviewExpe/PreviewExpe.test.tsx
@@ -4,19 +4,4 @@ import PreviewExpe from './PreviewExpe';
 
 import { expe1 } from '../../data-for-test';
 
-
-describe('Test PreviewExpe component', () => {
-    it('should call callback on click', () => {
-        const mockCallBack = jest.fn();
-        render(
-            <PreviewExpe
-                expedition={expe1}
-                onExpeditionClicked={mockCallBack}
-            />
-        );
-
-        fireEvent.click(screen.getByText(expe1.name));
-
-        expect(mockCallBack).toHaveBeenCalledTimes(1);
-    });
-});
+describe('Test PreviewExpe component', () => {});
--- src/components/PreviewExpe/PreviewExpe.tsx
+++ src/components/PreviewExpe/PreviewExpe.tsx
@@ -10,33 +10,7 @@ interface Props {
 }
 
 const PreviewExpe = ({ expedition, onExpeditionClicked }: Props) => {
-    return (
-        <div
-            className={classnames('card', 'mb-4', style.PreviewExpe)}
-            onClick={() => onExpeditionClicked()}
-        >
-            <div className="card-content">
-                <div className={classnames('content', style.content)}>
-                    <h1>{expedition.name}</h1>
-                    <span
-                        className={classnames('tag', style.tag, {
-                            'is-success': expedition.level == 'Facile',
-                            'is-warning': expedition.level == 'Moyen',
-                            'is-danger': expedition.level == 'Difficile',
-                        })}
-                    >
-                        {expedition.level}
-                    </span>
-                    <div>{expedition.description}</div>
-                    <div>
-                        <span className={'tag is-info'}>{expedition.placesCount} places</span>
-                        &nbsp;
-                        <span className={'tag is-info'}>{expedition.country}</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+    return <li>{expedition.name}</li>;
 };
 
 export default PreviewExpe;
